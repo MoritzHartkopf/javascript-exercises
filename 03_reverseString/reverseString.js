419abb735c40ebe057c653ed494044b086c96e75
MoritzHartkopf <106618531+MoritzHartkopf@users.noreply.github.com>
2023-05-02 16:10:04 +0200

    js 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let reversedString = "";
+    let posOfLastLetter = inputString.length -1 ;
+    for (let i = posOfLastLetter; i >= 0; i--)
+    {
+    reversedString = reversedString + inputString.charAt(i)
+    
+    }
+    return reversedString;   
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
