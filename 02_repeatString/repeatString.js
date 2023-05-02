8d6827af57a909dcd996466ecd8eb42e468f6b6b
MoritzHartkopf <106618531+MoritzHartkopf@users.noreply.github.com>
2023-05-02 15:30:54 +0200

    ex 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(message, numberOfRepetitions) {
+   
+    let repeatedString = "";
 
-};
+    if (numberOfRepetitions < 0){
+        return "ERROR"
+    }
+    else
+    {
+    }
+        for (let i = 0; i < numberOfRepetitions; i++){
+            repeatedString += message
+        }
+        return repeatedString
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
