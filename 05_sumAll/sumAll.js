117dba48426483463630943e203d6f82b39edee0
MoritzHartkopf <106618531+MoritzHartkopf@users.noreply.github.com>
2023-05-03 13:19:26 +0200

    sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
 
+// function that takes two numbers and sums them and the numbers in between
+const sumAll = function(numberA, numberB) {
+    let sum = 0;
+
+  // works if first number is bigger than second one 
+
+    if (numberA > numberB){
+        [numberA, numberB] = [numberB, numberA];
+    }
+
+// Error if element is non number character 
+    if (typeof numberA != "number" || typeof numberB != "number")
+    {
+        return "ERROR"
+}
+
+// ERROR if one of the numbers or both are negativ
+    if (numberA <0 || numberB < 0)
+    {
+     return "ERROR";
+    } 
+    
+
+// Actual summing 
+    for (i = numberA; i <= numberB; i++)
+    { 
+        sum += i;
+    
+    }
+    return sum;
 };
 
 // Do not edit below this line
