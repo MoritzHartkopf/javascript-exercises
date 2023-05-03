208543b1950091f74b9faeeae606f94113ec859f
MoritzHartkopf <106618531+MoritzHartkopf@users.noreply.github.com>
2023-05-03 14:06:21 +0200

    07 done, all js ecerscises finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+
+// converts F to C
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit -32) * 5/9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// converts C to F
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = celsius * 9/5 + 32; 
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
